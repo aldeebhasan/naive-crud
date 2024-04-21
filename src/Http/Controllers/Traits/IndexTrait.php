9401f083ce06b592d65b344aded7d1fa03ceac34
Hasan Deeb <h.deeb@zkood.com>
2024-04-21 09:57:53 +0300

    add search trait

--- src/Http/Controllers/Traits/IndexTrait.php
+++ src/Http/Controllers/Traits/IndexTrait.php
@@ -52,7 +52,7 @@ trait IndexTrait
 
         if ($this->paginated) {
             return [
-                'items' => $resource::collectionCustom($items->items())->toArray(),
+                'items' => $resource::collectionCustom($items->items(), $this->user)->toArray(),
                 'meta' => Arr::except($items->toArray(), [
                     'data', 'first_page_url', 'last_page_url', 'prev_page_url', 'next_page_url', 'links',
                 ]),
