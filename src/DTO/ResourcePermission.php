4d67dde80b1c48139ec9d296f780804b2ef9630a
Hasan Deeb <h.deeb@zkood.com>
2024-05-30 08:47:08 +0300

    wip: handle string in ResourcePermission

--- src/DTO/ResourcePermission.php
+++ src/DTO/ResourcePermission.php
@@ -5,21 +5,23 @@ namespace Aldeebhasan\NaiveCrud\DTO;
 use Aldeebhasan\NaiveCrud\Traits\Makable;
 use Spatie\Permission\Models\Permission;
 
-/**@method  static ResourcePermission make(string $guard, string $resource, array $actions = []) */
+/**@method  static ResourcePermission make(string $guard, string $resource, array|string $actions = []) */
 final readonly class ResourcePermission
 {
     use Makable;
 
     public string $resource;
+    public array|string $actions;
 
     public function __construct(
         public string $guard,
-        string $resource,
-        public array $actions
+        string        $resource,
+        array|string  $actions
     )
     {
         $modelClass = class_basename($resource);
         $this->resource = str($modelClass)->plural()->snake('_')->toString();
+        $this->actions = is_string($actions) ? [$actions] : $actions;
     }
 
     public function register(): void
